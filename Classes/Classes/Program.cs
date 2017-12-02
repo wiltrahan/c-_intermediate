ee535d272afcdc01c03c4bee3f232cf084ca4e2c
wiltrahan <twilorip@gmail.com>
2017-12-02 12:02:28 -0500

    section 2_lecture 6

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 
 namespace Classes
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+    
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var person = Person.Parse("John");
+            
+            person.Introduce("Willis");
         }
     }
 }
