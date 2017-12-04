94733fd55be2b2d8287dfaa4ccc93959a535ecf5
wiltrahan <twilorip@gmail.com>
2017-12-04 17:32:47 -0500

    section 2_lecture 7

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Constructors
+{
+   
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var customer = new Customer(1, "Wil");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
