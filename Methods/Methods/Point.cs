627331af8c0fd838a6e34fa4f996a7bfcab5f14d
wiltrahan <twilorip@gmail.com>
2017-12-04 18:59:39 -0500

    section 2_lecture 9

---
+++ Methods/Methods/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Methods
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x; //10
+            this.Y = y; //20
+        }
+
+        public void Move(int x, int y)
+        {
+            this.X = x; //40
+            this.Y = y; //60
+        }
+
+        public void Move(Point newLocation)
+        {
+            if(newLocation == null)
+                throw new ArgumentNullException("newLocation");
+            
+            Move(newLocation.X, newLocation.Y);
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Methods
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var number = int.Parse("abc");
+            int number;
+            var result = int.TryParse("abc", out number);
+            if(result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversion Failed");
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[]{ 1, 2, 3, 4, 5})); 
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(new Point(40, 60));
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occured.");
+            }
+        }
+    }
+}
