ed7857ab37c73d46f445ff2a85f77235183c3d7f
wiltrahan <twilorip@gmail.com>
2017-12-04 19:29:06 -0500

    section 2_lecture 11

---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthDate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthDate()
+        {
+            return _birthdate;
+        }
+    }
+    
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthDate(new DateTime(1979, 8, 29));
+            Console.WriteLine(person.GetBirthDate());
+        }
+    }
+}
