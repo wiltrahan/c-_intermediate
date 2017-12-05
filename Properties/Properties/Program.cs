e79ee10596e966cccf8c77c3ea446ed8bdf1f2e3
wiltrahan <twilorip@gmail.com>
2017-12-04 20:04:04 -0500

    section 2_lecture 12

---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Properties
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1979, 8, 29));
+            Console.WriteLine(person.Age);
+        }
+    }
+}
