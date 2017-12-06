e8cb8557f2a6dcc408ef516e619052dcd38d4674
wiltrahan <twilorip@gmail.com>
2017-12-06 16:33:02 -0500

    section 3_lecture 17

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
+        }
+    }
+}
