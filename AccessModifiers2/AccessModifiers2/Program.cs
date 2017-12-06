6a74c5b9d612eb3ef182167fd4b299520807be9c
wiltrahan <twilorip@gmail.com>
2017-12-06 17:50:44 -0500

    section 4_lecture 21

---
+++ AccessModifiers2/AccessModifiers2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Amazon;
+
+namespace AccessModifiers2
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var customer = new Customer();
+            //Amazon.RateCalculator calculator = new RateCalculator();
+        }
+    }
+}
