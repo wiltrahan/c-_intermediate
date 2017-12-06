970dd562bad1eb5b53f80233f7dc523ed1858338
wiltrahan <twilorip@gmail.com>
2017-12-06 18:58:02 -0500

    section 4_lecture 23

---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Casting
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Shape shape = new Text();
+            Text text = (Text) shape;
+        }
+    }
+}
