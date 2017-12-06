67b164c61618e4a237efb3a9cb6cfc326875127a
wiltrahan <twilorip@gmail.com>
2017-12-06 16:51:44 -0500

    section 3_lecture 18

---
+++ Composition/Composition/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Composition
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new Logger());
+            
+            var logger = new Logger();
+            var installer = new Installer(logger);
+            
+            dbMigrator.Migrate();
+            
+            installer.Install();
+        }
+    }
+}
