0701defedef8f5426c45f1c1e0445f77cf169279
wiltrahan <twilorip@gmail.com>
2017-12-07 15:21:08 -0500

    section 4_lecture 24

---
+++ Boxing/Boxing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Boxing
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Wil");
+            list.Add(DateTime.Today);
+            
+            var anotherList = new List<int>();
+            var names = new List<string>();
+            names.Add("Frankie");
+        }
+    }
+}
