2f6a97a0f03331d6ce1011b0c46ad6e65e741c39
wiltrahan <twilorip@gmail.com>
2017-12-08 16:20:35 -0500

    section 6_lecture 34

---
+++ Extensibility/Extensibility/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Extensibility
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new ConsoleLogger());
+            dbMigrator.Migrate();
+        }
+    }
+}
