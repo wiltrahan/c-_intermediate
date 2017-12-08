e4c505933203ae9e82cb45eb37996b5cbeb56454
wiltrahan <twilorip@gmail.com>
2017-12-07 19:53:28 -0500

    section 5_lecture 28

---
+++ AbstractClasses/AbstractClasses/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace AbstractClasses
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var circle = new Circle();
+            circle.Draw();
+            
+            var rectangle = new Rectangle();
+            rectangle.Draw();
+        }
+    }
+}
