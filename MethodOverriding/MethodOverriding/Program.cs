3c64fe4c07ba56a933d2afadc3d2c8d60b2f7b72
wiltrahan <twilorip@gmail.com>
2017-12-07 19:18:28 -0500

    section 5_lecture 27

---
+++ MethodOverriding/MethodOverriding/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MethodOverriding
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var shapes = new List<Shape>();
+            shapes.Add(new Circle());
+            shapes.Add(new Rectangle());
+            
+            var canvas = new Canvas();
+            canvas.DrawShapes(shapes);
+            
+        }
+    }
+}
