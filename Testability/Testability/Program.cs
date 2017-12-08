c21291c2dc0e07eb599a70b4140ff35613f8c7b2
wiltrahan <twilorip@gmail.com>
2017-12-08 14:37:28 -0500

    section 6_lecture 33

---
+++ Testability/Testability/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Testability
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var orderProcessor = new OrderProcessor(new ShippingCalculator());
+            var order = new Order {DatePlaced = DateTime.Now, TotalPrice = 100f};
+            orderProcessor.Process(order);
+        }
+    }
+}
